f41b517a5dd548813cc09db640eb6759db3beb52
BeygonK <kipkoechbeygon@gmail.com>
2024-10-31 16:33:09 +0300

    Created necessary pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-dark-600 '>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
